a87b87f709b44483b4254f058b30a48dc29e1ea4
harabalos <babismetaxas000@gmail.com>
2022-01-06 02:43:07 +0200

    changed it completely and fixed it

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,126 +1,138 @@
-from random import randint
+
+
+import random
 
 
 def display_board(board):
-    print('     |', '    ', '|')
-    print(' ', board[1], '    ', board[2], '   ', board[3])
-    print('_____|', '____', '|_____')
-    print('     |', '    ', '|')
-    print(' ', board[4], '    ', board[5], '   ', board[6])
-    print('_____|', '____', '|_____')
-    print('     |', '    ', '|')
-    print(' ', board[7], '    ', board[8], '   ', board[9])
-    print('     |', '    ', '|')
+    print('\n'*100)
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
 
 
 def player_input():
-    choice = 'wrong'
-    correct_items = ['X', 'x', 'O', 'o']
-    while choice not in correct_items:
-        choice = input('Please enter X or O: ')
-        if choice not in correct_items:
-            print('You can only enter X or O')
-    return choice
+    marker = ''
+
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
 
 
-def user_position():
-    position = 'wrong'
-    acceptable_range = range(1, 9)
-    within_range = False
-    while position.isdigit() == False or within_range == False:
-        position = input('Please enter a number between 1 - 9: ')
-        if position.isdigit() == False:
-            print('Sorry that is not a digit')
-        if position.isdigit() == True:
-            if int(position) in acceptable_range:
-                within_range = True
-            else:
-                print('Your are out of acceptable range (1-9)')
-                within_range = False
-    return int(position)
+def place_marker(board, marker, position):
+    board[position] = marker
 
 
-def place_marker(board, choice, position):
-    board[position] = choice
-    return board
+def win_check(board, mark):
 
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or
+            (board[4] == mark and board[5] == mark and board[6] == mark) or
 
-def replay():
-    choice = 'wrong'
-    while choice not in ['Y', 'y', 'N', 'n']:
-        choice = input('Do you want to play again (Y or N)?: ')
-        if choice not in ['Y', 'y', 'N', 'n']:
-            print('I dont understand please put Y or N')
-        if choice == 'Y' or choice == 'y':
-            return True
-        elif choice == 'N' or choice == 'n':
-            return False
+            (board[1] == mark and board[2] == mark and board[3] == mark) or
+            (board[7] == mark and board[4] == mark and board[1] == mark) or
+            (board[8] == mark and board[5] == mark and board[2] == mark) or
 
+            (board[9] == mark and board[6] == mark and board[3] == mark) or
 
-def win_check(board, choise):
-    for i in board:
-        if board[1] == choise:
-            if board[2] == choise and board[3] == choise:
-                return True
-            elif board[4] == choise and board[7] == choise:
-                return True
-            elif board[5] == choise and board[9] == choise:
-                return True
-        elif board[2] == choise:
-            if board[5] == choise and board[8] == choise:
-                return True
-        elif board[3] == choise:
-            if board[5] == choise and board[7] == choise:
-                return True
-            elif board[6] == choise and board[9] == choise:
-                return True
-        elif board[4] == choise:
-            if board[5] == choise and board[6] == choise:
-                return True
-        elif board[7] == choise:
-            if board[8] == choise and board[9] == choise:
-                return True
+            (board[7] == mark and board[5] == mark and board[3] == mark) or
+            (board[9] == mark and board[5] == mark and board[1] == mark))
 
 
 def choose_first():
-    first = randint(1, 2)
-    print(f'player {first} will go first')
+    if random.randint(0, 1) == 0:
+        return 'Player 2'
+    else:
+        return 'Player 1'
 
 
 def space_check(board, position):
-    if ' ' in board:
-        return True
-    else:
-        return False
+
+    return board[position] == ' '
 
 
 def full_board_check(board):
-    s = 0
-    for i in board:
-        if i == 'O' or i == 'X':
-            s = s + 1
-        elif i == '#':
-            continue
-    if s == 9:
-        return True
-    else:
-        return False
+    for i in range(1, 10):
+        if space_check(board, i):
+            return False
+    return True
 
 
 def player_choice(board):
-    position = 'wrong'
-    acceptable_range = range(1, 9)
-    within_range = False
-    while position.isdigit() == False or within_range == False:
-        position = input('Please enter a number between 1 - 9: ')
-        if position.isdigit() == False:
-            print('Sorry that is not a digit')
-        if position.isdigit() == True:
-            if int(position) in acceptable_range:
-                within_range = True
-            else:
-                print('Your are out of acceptable range (1-9)')
-                within_range = False
-    if space_check:
+    position = 0
 
-        return int(position)
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9) '))
+
+    return position
+
+
+def replay():
+
+    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
+
+print('Welcome to Tic Tac Toe!')
+
+while True:
+    theBoard = [' '] * 10
+    player1_marker, player2_marker = player_input()
+    turn = choose_first()
+    print(turn + ' will go first.')
+
+    play_game = input('Are you ready to play? Enter Yes or No.')
+
+    if play_game.lower()[0] == 'y':
+        game_on = True
+    else:
+        game_on = False
+
+    while game_on:
+        if turn == 'Player 1':
+
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player1_marker, position)
+
+            if win_check(theBoard, player1_marker):
+                display_board(theBoard)
+                print('Congratulations! You have won the game!')
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('The game is a draw!')
+                    break
+                else:
+                    turn = 'Player 2'
+
+        else:
+
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player2_marker, position)
+
+            if win_check(theBoard, player2_marker):
+                display_board(theBoard)
+                print('Player 2 has won!')
+                game_on = False
+            else:
+                if full_board_check(theBoard):
+                    display_board(theBoard)
+                    print('The game is a draw!')
+                    break
+                else:
+                    turn = 'Player 1'
+
+    if not replay():
+        break
